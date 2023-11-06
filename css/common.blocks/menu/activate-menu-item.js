476c43b12023195c5ba58ee98dc7f68b821de1fa
Max <for_maxim@bk.ru>
2023-11-07 01:29:33 +0300

    feat: add page load

--- css/common.blocks/menu/activate-menu-item.js
+++ css/common.blocks/menu/activate-menu-item.js
@@ -2,5 +2,4 @@ document.querySelectorAll(".menu__item")
     .forEach(t => {
         if (t.href === document.location.origin + document.location.pathname)
             t.classList.add("menu__item__active")
-        console.log(t)
     })
