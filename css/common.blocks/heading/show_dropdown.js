1eac1369aea9ecc91ba8dc879d92a2a1afe42dba
Max <for_maxim@bk.ru>
2023-11-06 23:17:59 +0300

    refactor: menu

--- css/common.blocks/heading/show_dropdown.js
+++ css/common.blocks/heading/show_dropdown.js
@@ -1,3 +1,16 @@
 function showDropdown() {
     document.querySelector(".heading__dropdown").classList.toggle("dropdown-show")
 }
+
+window.addEventListener('click', function (e) {
+    let main = document.querySelector(".heading__profile")
+    if (!main.contains(e.target)) {
+        let dropdowns = document.getElementsByClassName("heading__dropdown");
+        for (let i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('dropdown-show')) {
+                openDropdown.classList.remove('dropdown-show');
+            }
+        }
+    }
+})
