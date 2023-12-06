c61f81ecb9b498295e8909c0ac518e08e9145e50
Max <for_maxim@bk.ru>
2023-12-06 12:38:45 +0300

    fix: bolding active page

--- css/common.blocks/menu/activate-menu-item.js
+++ css/common.blocks/menu/activate-menu-item.js
@@ -1,5 +1,9 @@
 document.querySelectorAll(".menu__item")
     .forEach(t => {
-        if (t.href === document.location.origin + document.location.pathname)
+            if (t.href === document.location.origin + document.location.pathname)
             t.classList.add("menu__item__active")
     })
+
+if (document.location.pathname==='/Web-5sem/') {
+    document.querySelector(".menu__first-item").classList.add("menu__item__active")
+}
