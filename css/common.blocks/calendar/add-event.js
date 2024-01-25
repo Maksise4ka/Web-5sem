915a51014231fbcfd194d7c0cdc5280e9863700a
Max <for_maxim@bk.ru>
2024-01-26 00:14:37 +0300

    feat: add add-event form

---
+++ css/common.blocks/calendar/add-event.js
@@ -0,0 +1,115 @@
+function getCurrentTimeRange() {
+    // TODO
+    return ["0:00", "23:00"]
+}
+
+function isWorkWeek() {
+    // TODO
+}
+
+//TODO
+function findRelevantTs(start, dayNumber) {
+    let cell= document.getElementById(`${start}_${dayNumber}`)
+
+    return cell
+}
+
+/**
+ * Конвертирование времени в формате "xx:xx" до минут
+ * @param time
+ * @returns {number}
+ */
+function convertTimeToMinutes(time) {
+    let units = time.split(":").map(u => Number(u))
+    return units[0] * 60 + units[1]
+}
+
+function calculateTimeSlotCount(startMinute, endMinute) {
+    return (endMinute - startMinute) / 30
+}
+
+// let event = {
+//     "customer": "",
+//     "master",
+//     "date": "",
+//     "start": "",
+//     "end": ""
+// }
+function displayEvent(event) {
+    let timeRange = getCurrentTimeRange().map(convertTimeToMinutes)
+    let rangeStart = timeRange[0]
+    let rangeEnd = timeRange[1]
+
+    let eventStart = convertTimeToMinutes(event.start)
+    let eventEnd = convertTimeToMinutes(event.end)
+
+    if (!(eventStart >= rangeStart ||eventEnd <= rangeEnd))
+        return
+
+    let timeSlotsCount = calculateTimeSlotCount(eventStart, eventEnd) // TODO corner cases
+    // let timeSlotsCount = 5
+    let cell = findRelevantTs(event.start, 1) // TODO
+
+    let template = document.getElementById("calendar-event")
+    let eventNode = template.content.cloneNode(true)
+    // let height = document.defaultView.getComputedStyle(cell).height.slice(0, -2) * timeSlotsCount
+    let height = cell.offsetHeight * timeSlotsCount - 5
+
+    eventNode.children[0].style.setProperty("height", `${height}px`)
+    cell.appendChild(eventNode)
+}
+
+function toggleCalendarShow() {
+    document.querySelector(".add-event").classList.toggle("add-event__show")
+    document.querySelector(".layout-overlay").classList.toggle("layout-overlay__calendar")
+}
+
+window.addEventListener('click', function (e) {
+    let menu = document.querySelector(".add-event")
+    let layoutOverlay = document.querySelector(".layout-overlay")
+    if (!menu.contains(e.target) && !e.target.matches(".calendar-settings__create-button") && layoutOverlay.classList.contains("layout-overlay__calendar")) {
+        toggleCalendarShow()
+    }
+})
+
+let event = {
+    "customer": "f",
+    "master": "f",
+    "date": "?",
+    "start": "2:30",
+    "end": "8:30"
+}
+
+displayEvent(event)
+
+document.querySelector(".add-event__create-button").addEventListener("click", event => {
+    let events = JSON.parse(localStorage.getItem("events"))
+    if (events === null)
+        events = []
+
+    let isValidForm = document.getElementById("add-event__form").checkValidity();
+    if (!isValidForm) {
+        return false
+    }
+
+    let start = document.getElementById("event-start").value
+    let end = document.getElementById("event-end").value
+    if (convertTimeToMinutes(start) >= convertTimeToMinutes(end)) {
+        alert("Начало события должно идти раньше конца")
+        return false
+    }
+
+    event.preventDefault()
+    let newEvent = {
+        "id": events.length + 1,
+        "customer": document.getElementById("name").value,
+        "master": document.getElementById("event-master").value,
+        "date": document.getElementById("date").value,
+        "start": start,
+        "end": end
+    }
+
+    events.push(newEvent)
+    localStorage.setItem("events", JSON.stringify(events))
+    toggleCalendarShow()
+})
