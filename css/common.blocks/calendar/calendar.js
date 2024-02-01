711804bcc4e3cba3245377b6cd81bc710020fee2
Max <for_maxim@bk.ru>
2024-02-01 22:26:24 +0300

    feat: add event intersect compatibility

--- css/common.blocks/calendar/calendar.js
+++ css/common.blocks/calendar/calendar.js
@@ -346,9 +346,95 @@ function getWeekday(date) {
 }
 
 /**
- * Отображает события, которые попадают в заданный диапазон календаря
+ * Считает ширину и смещение вправо для карточки события
+ * @param events
+ * @returns {*} - events, но с добавлением двух полей width - доля занимаемой ячейки 0
  */
-function displayEventsWithFilter() {
+function calculateEventsCardWidth(events) {
+    const daysCount = 7
+    const hoursCount = 24
+
+    function timeToIndex(time) {
+        let units = time.split(":").map(u => Number(u))
+        return units[0] * 2 + units[1] / 30
+    }
+
+    function prepareTable(events) {
+
+        let table = new Array(daysCount)
+        for (let i = 0; i < daysCount; ++i) {
+            table[i] = new Array(hoursCount * 2)
+            for (let j = 0; j < hoursCount * 2; ++j)
+                table[i][j] = []
+        }
+
+        events.forEach(e => {
+            let weekday = getWeekday(new Date(e.date))
+            let start = timeToIndex(e.start)
+            let end = timeToIndex(e.end)
+            for (let i = start; i < end; ++i) {
+                table[weekday][i].push(e)
+            }
+        })
+
+        for (let i = 0; i < daysCount; ++i)
+            for (let j = 0; j < hoursCount * 2; ++j)
+                table[i][j].sort((a, b) => timeToIndex(a.start) - timeToIndex(b.start))
+
+        return table
+    }
+
+    let table = prepareTable(events)
+
+    for (let i = 0; i < daysCount; ++i) {
+        for (let j = 0; j < hoursCount * 2; ++j) {
+            for (let k = 0; k < table[i][j].length; ++k) {
+                let e = table[i][j][k]
+                e.compress = e.compress === undefined ? 1 : e.compress
+                e.compress = Math.max(e.compress, table[i][j].length)
+            }
+        }
+    }
+
+    for (let i = 0; i < daysCount; ++i) {
+        let compressMap = []
+
+        for (let j = 0; j < hoursCount * 2; ++j) {
+            if (table[i][j].length === 0)
+                continue
+
+            let compress = table[i][j].reduce((prev, cur) => Math.max(prev, cur.compress), 0)
+            let count = compressMap.reduce((prev, cur) => cur === undefined ? prev : prev + cur, 0)
+            if (count === 0) {
+                compressMap = new Array(compress)
+            }
+            for (let k = 0; k < table[i][j].length; ++k) {
+                let e = table[i][j][k]
+                e.compress = compress
+                if (timeToIndex(e.start) === j) {
+                    for (let l = 0; l < compress; ++l) {
+                        if (compressMap[l] !== 1) {
+                            compressMap[l] = 1
+                            e.offset = l
+                            break;
+                        }
+                    }
+                }
+            }
+
+            for (let k = 0; k < table[i][j].length; ++k) {
+                let e = table[i][j][k]
+                if (timeToIndex(e.end) === j + 1) {
+                    compressMap[e.offset] = 0
+                }
+            }
+        }
+    }
+
+    return events
+}
+
+function filterEvents() {
     let range = getCurrentTimeRange().map(x => x * 60)
 
     function filterEvent(event) {
@@ -368,7 +454,17 @@ function displayEventsWithFilter() {
     if (events === null)
         events = []
 
-    return events.filter(filterEvent).forEach(displayEvent)
+    return events.filter(filterEvent)
+}
+
+/**
+ * Отображает события, которые попадают в заданный диапазон календаря
+ */
+function displayEventsWithFilter() {
+    let events = filterEvents()
+    events = calculateEventsCardWidth(events)
+
+    return events.forEach(displayEvent)
 }
 
 
@@ -393,11 +489,13 @@ function displayEvent(event) {
     let template = document.getElementById("calendar-event")
     let eventNode = template.content.cloneNode(true)
     let height = cell.offsetHeight * timeSlotsCount - 5
-    let width = cell.offsetWidth - 5
+    let width = (cell.offsetWidth - 5) / event.compress
+    let marginLeft = cell.offsetWidth / event.compress * event.offset
     // let height = document.defaultView.getComputedStyle(cell).height.slice(0, -2) * timeSlotsCount
 
     eventNode.children[0].style.setProperty("height", `${height}px`)
     eventNode.children[0].style.setProperty("width", `${width}px`)
+    eventNode.children[0].style.setProperty("margin-left", `${marginLeft}px`)
     eventNode.children[0].innerHTML = event.description
     cell.appendChild(eventNode)
 }
